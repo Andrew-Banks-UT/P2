ae6be7826cb44df4e65319813f376e1b2e72c890
Andrew-Banks-UT <77769993+Andrew-Banks-UT@users.noreply.github.com>
2021-11-08 23:25:04 -0500

    Update lab2test.c

--- lab2test.c
+++ lab2test.c
@@ -2,25 +2,26 @@
 #include "user.h"
 
 int main() {
-	int ret;
-	//Protect and unprotect an address
-	if((ret = mprotect((void *)4096, 4096))==-1){
-		printf(1, "mprotect failed %d\n", ret);
-	}
-	printf(1, "mprotect called\n");
-	if((ret = munprotect((void *)4096, 4096))==-1){
-		printf(1, "munprotect failed %d\n", ret);
-	}
-	printf(1, "munprotect called, attempting to overwrite...\n");
-	//Check if it was sucessfully unprotected
-	*(int *)4096 = 1;
-	printf(1, "Overwrite successful, address=%d\n",  *(int *)4096);
-	//Protect it again and try to write to it, this should result in a trap error and terminate the program
-	if((ret = mprotect((void *)4096, 4096))==-1){
-		printf(1, "mprotect failed %d\n", ret);
-	}
-	printf(1, "mprotect called, attempting to overwrite...\n");
-	*(int *)4096 = 2;
-	printf(1, "Overwrite successful, mprotect failed\n");
-	exit();
+        int ret;
+        int *addr=(int*) 4096;
+        //Protect and unprotect an address
+        if((ret = mprotect((void*)addr, 1))==-1){
+                printf(1, "mprotect failed %d\n", ret);
+        }
+        printf(1, "mprotect called\n");
+        if((ret = munprotect((void *)addr, 1))==-1){
+                printf(1, "munprotect failed %d\n", ret);
+        }
+        printf(1, "munprotect called, attempting to overwrite...\n");
+        //Check if it was sucessfully unprotected
+        *addr = 1;
+        printf(1, "Overwrite successful, address=%d\n",  *(int *)4096);
+        //Protect it again and try to write to it, this should result in a trap error and terminate the program
+        if((ret = mprotect((void *)addr, 1))==-1){
+                printf(1, "mprotect failed %d\n", ret);
+        }
+        printf(1, "mprotect called, attempting to overwrite...\n");
+        *addr = 2;
+        printf(1, "Overwrite successful, mprotect failed\n");
+        exit();
 }
