e23c1a84e5dfa61e783ac3a49c49f12b10e5d04c
Andrew-Banks-UT <77769993+Andrew-Banks-UT@users.noreply.github.com>
2021-11-08 22:00:26 -0500

    Update lab2test.c

--- lab2test.c
+++ lab2test.c
@@ -4,11 +4,11 @@
 int main() {
 	int ret;
 	//Protect and unprotect an address
-	if((ret = mprotect((void *)4096, 4096))==1){
+	if((ret = mprotect((void *)4096, 4096))==-1){
 		printf(1, "mprotect failed %d\n", ret);
 	}
 	printf(1, "mprotect called\n");
-	if((ret = munprotect((void *)4096, 4096))==1){
+	if((ret = munprotect((void *)4096, 4096))==-1){
 		printf(1, "munprotect failed %d\n", ret);
 	}
 	printf(1, "munprotect called, attempting to overwrite...\n");
@@ -16,7 +16,7 @@ int main() {
 	*(int *)4096 = 1;
 	printf(1, "Overwrite successful, address=%d\n",  *(int *)4096);
 	//Protect it again and try to write to it, this should result in a trap error and terminate the program
-	if((ret = mprotect((void *)4096, 4096))==1){
+	if((ret = mprotect((void *)4096, 4096))==-1){
 		printf(1, "mprotect failed %d\n", ret);
 	}
 	printf(1, "mprotect called, attempting to overwrite...\n");
